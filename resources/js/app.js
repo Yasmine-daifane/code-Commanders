497753159fbb50062134ba11a8e8f0a2dcfa0992
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-05-16 17:44:55 +0100

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+
+import 'bootstrap';
+// Import jQuery
+import 'jquery/dist/jquery';
+// Import AdminLTE
+import "admin-lte/dist/js/adminlte";
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle';
+import 'admin-lte/dist/js/adminlte';
